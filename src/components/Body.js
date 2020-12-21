e4b96e0a801e2aabb6fa44bd23b5add7778aaeeb
bertin <ruhimbazab@gmail.com>
2020-12-21 04:39:42 -0800

    react-app
    different components
    
    react-app

---
+++ src/components/Body.js
@@ -0,0 +1,15 @@
+import react, {Component} from 'react';
+
+class Body extends Component  {
+
+    render() {
+        return (
+            <div>
+                <strong>This is the body</strong>
+            </div>
+
+        );
+    }
+}
+
+export default Body;
